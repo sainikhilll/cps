022842bbac8d6036881a224153933294e49a6722
Sahith45 <sahithcherry99@gmail.com>
2021-11-09 18:11:12 +0530

    rest_url

--- cps-frontend/src/app/service/trip.service.ts
+++ cps-frontend/src/app/service/trip.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Trip } from '../model/trip';
 import { Observable } from 'rxjs';
+import { REST_URL } from './common';
 @Injectable({
   providedIn: 'root'
 })
 export class TripService {
-  url = "http://localhost:8080/trips";
+  url = REST_URL+"trips";
 
   constructor(private _http: HttpClient) { }
 
