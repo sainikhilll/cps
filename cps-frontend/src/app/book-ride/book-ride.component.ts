c26aa4b70acb6fd028f4532a994d41c19eeb9e6d
amaravadi sainikhil <amaravadi.sainikhil@gmail.com>
2021-11-10 15:07:15 +0530

    Deployment in progress

--- cps-frontend/src/app/book-ride/book-ride.component.ts
+++ cps-frontend/src/app/book-ride/book-ride.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TripBooking } from '../model/trip-booking';
 
 @Component({
   selector: 'app-book-ride',
@@ -8,7 +9,50 @@ import { Component, OnInit } from '@angular/core';
 export class BookRideComponent implements OnInit {
 
   constructor() { }
-
+  bookingTrip:TripBooking={
+    id: 0,
+    seatsBooked: 0,
+    trip: {
+      id: 3,
+      origin: "",
+      destination: "",
+      departureTime: "",
+      price: 0,
+      date: new Date(2021,10,1),
+    owner: {
+      name:"",
+      address:"",
+      mobile:"",
+      licenceNumber:"",
+      licenceUrl:"",
+      aadharNumber:"",
+      aadharUrl:"",
+      user:{ id: 0,
+        email: "",
+        password:"",
+        role:""	},
+      ownerPreference:{
+        id:1,
+        music:"",
+        smoking:"",
+        petsAllowed:""
+      },
+      status:""
+    },
+      numberOfPassengers: 0,    
+      numberOfSeatsAvailable: 0,
+      carType: ""
+    },
+    passenger:{  id: 1,
+    name: "",
+    mobileNumber: "",
+    dateOfBirth:new Date(2021,10,1) ,
+    user:{ id: 0,
+      email: "",
+      password:"",
+      role:""	}
+  }
+  }
   ngOnInit(): void {
   }
 
