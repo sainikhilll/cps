018053c2ab16e50845be7c066e241769c9557004
Sibghatullah <mdsibghat28@gmail.com>
2021-11-11 13:34:55 +0530

    change in port

--- cps-rest/src/main/java/com/revature/cpsrest/controller/UserController.java
+++ cps-rest/src/main/java/com/revature/cpsrest/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.revature.cpsrest.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
