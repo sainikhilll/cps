7eed54d84b3e57dc94d503626d6a357eb90e8c8c
Sahith45 <sahithcherry99@gmail.com>
2021-11-22 11:05:38 +0530

    admin-view-cancel-ride

--- cps-frontend/src/app/admin-view-rides/admin-view-rides.component.ts
+++ cps-frontend/src/app/admin-view-rides/admin-view-rides.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip } from '../model/trip';
+import { AdminCancelRidesService } from '../service/admin-cancel-rides.service';
+import { AdminViewRidesService } from '../service/admin-view-rides.service';
 
 @Component({
   selector: 'app-admin-view-rides',
@@ -6,10 +9,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-view-rides.component.css']
 })
 export class AdminViewRidesComponent implements OnInit {
+  trips :any ={
+    "id": 0,
+    "origin": "",
+    "destination": "",
+    "departureTime": "",
+    "price":0,
+    "date": "",
+    "numberOfPassengers": 0,
+    "numberOfSeatsAvailable": 0,
+    "carType": "",
+    "owner": {
+        "id": 0,
+        "name": "",
+        "address": "",
+        "mobile": "",
+        "licenceNumber": "",
+        "licenceUrl": "",
+        "aadharNumber": "",
+        "aadharUrl": "",
+        "status": "",
+        "user": {
+            "id": 0,
+            "email": "",
+            "password": "",
+            "role": "",
+            "blacklisted": ""
+        },
+        "ownerPreference": {
+            "id": 0,
+            "music": "",
+            "smoking": "",
+            "petsAllowed": ""
+        }
+    },
+    "status": ""
+}
 
-  constructor() { }
+currentDate = new Date();
+
+
+
+  constructor(private _service:AdminViewRidesService) { }
 
   ngOnInit(): void {
+    this._service.restTrips().subscribe(
+      data => {
+        this.trips=data;
+      }
+    );
+  }
+
+
+  changeStatus(id: number){
+    this._service.restStatus(id).subscribe(
+      data=>{
+        console.log(id);
+        console.log("cancelled");
+        this.ngOnInit();
+      }
+    )
   }
 
 }
--- cps-frontend/src/app/service/admin-view-rides.service.ts
+++ cps-frontend/src/app/service/admin-view-rides.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Trip } from '../model/trip';
+import { REST_URL } from './common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminViewRidesService {
+  url=REST_URL+"trips";
+ 
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  public restTrips():Observable<any>{
+    return this._http.get<any>(this.url);
+  }
+
+  public restStatus(id: number):Observable<any>{
+    return this._http.get<any>(REST_URL + "canceltrips/" +  4);
+  }
+
 }
