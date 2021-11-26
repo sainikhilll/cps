378f4fbb7feb0c3e70b5da3b8d9bf8725498ff06
amaravadi sainikhil <amaravadi.sainikhil@gmail.com>
2021-11-26 16:51:49 +0530

    Database shifted to revature db

--- cps-frontend/src/app/service/common.ts
+++ cps-frontend/src/app/service/common.ts
@@ -1,2 +1,2 @@
-// export const REST_URL = "http://54.146.205.172:2025/";
-export const REST_URL = "http://localhost:8080/";
+ export const REST_URL = "http://54.146.205.172:2025/";
+//export const REST_URL = "http://localhost:8080/";
